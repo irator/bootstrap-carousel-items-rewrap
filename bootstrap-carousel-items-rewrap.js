30b556c610833a5778b8ae3c77d2e1b38b66dc80
irator <irator.ru@gmail.com>
2017-01-10 11:20:02 +0300

    Create bootstrap-carousel-items-rewrap.js

---
+++ bootstrap-carousel-items-rewrap.js
@@ -0,0 +1,27 @@
+var item    = '#products_carousel div.item',
+    newItem = '<div class="item" />',
+    div     = $('#products_carousel div.product-block-item'),
+    dl      = div.length;
+
+function rewrap() {
+  $(item).contents().unwrap();
+  dl = div.length;
+  if($(window).width() >= 768){
+    for (var i = 0; i < dl; i = i + 3) {
+      div.slice(i, i + 3).wrapAll(newItem)
+    }
+  }
+  if($(window).width() < 768){
+    for (var i = 0; i < dl; i = i + 1) {
+      div.slice(i, i + 1).wrapAll(newItem)
+    }
+  }
+  $(item).first().addClass('active');
+  $('.carousel').carousel()
+}
+
+rewrap();
+
+$(window).on('resize',function() {
+  rewrap()
+});
